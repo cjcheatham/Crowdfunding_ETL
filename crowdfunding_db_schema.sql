296f344040d7a62d4a786ae71e949c00106644ce
cjcheatham <cjcheatham0500@gmail.com>
2023-11-28 22:38:48 -0800

    Updated Schema.

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -7,34 +7,31 @@ DROP TABLE IF EXISTS campaign;
 
 -- Create tables
 CREATE TABLE contacts (
-    contact_id VARCHAR(10) NOT NULL,
+    contact_id INTEGER PRIMARY KEY NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-        CONSTRAINT "primary_contacts" PRIMARY KEY ("contact_id")
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE category (
-    category_id VARCHAR(10) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-        CONSTRAINT "primary_category" PRIMARY KEY ("category_id")
+    category_id VARCHAR(10) PRIMARY KEY NOT NULL,
+    category VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR(10) NOT NULL, 
-    subcategory VARCHAR(50) NOT NULL,
-        CONSTRAINT "primary_subcategory" PRIMARY KEY ("subcategory_id")
+    subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL, 
+    subcategory VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE campaign (
-    cf_id INTEGER NOT NULL,
+    cf_id INTEGER PRIMARY KEY NOT NULL,
     contact_id INTEGER NOT NULL,
     company_name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL, 
     goal FLOAT NOT NULL,
     pledged FLOAT NOT NULL,
-    outcome VARCHAR(50) NOTN NULL,
+    outcome VARCHAR(50) NOT NULL,
     backers_count INTEGER NOT NULL,
     country VARCHAR(10) NOT NULL, 
     currency VARCHAR(10),
@@ -42,12 +39,7 @@ CREATE TABLE campaign (
     end_date DATE NOT NULL, 
     category_id VARCHAR(10),
     subcategory_id VARCHAR(10),
-        CONSTRAINT "primary_campaign" PRIMARY KEY ("cf_id")
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
 );
-
-ALTER TABLE "campaign" ADD CONSTRAINT "foreigncamp_con_id" 
-FOREIGN KEY("contact_id") REFERENCES "contacts" ("contact_id");
-ALTER TABLE "campaign" ADD CONSTRAINT "foreigncamp_cat_id" 
-FOREIGN KEY("category_id") REFERENCES "category" ("category_id");
-ALTER TABLE "campaign" ADD CONSTRAINT "foreigncamp_subcat_id" 
-FOREIGN KEY("subcategory_id") REFERENCES "subcategory" ("subcategory_id");
